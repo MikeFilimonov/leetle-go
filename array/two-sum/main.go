917fc6c175f35c1ad663d53d8096b5e21a112e87
MikeFilimonov <filimonov.michael@gmail.com>
2026-04-15 08:55:05 +0300

    20260415-easy

---
+++ array/two-sum/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+func main() {
+
+	input := []int{2, 7, 11, 15}
+
+	fmt.Println("the indices are: ", twoSum(input, 9))
+
+}
+
+func twoSum(input []int, target int) []int {
+
+	result := make([]int, 0)
+
+	for i, v := range input {
+
+		nextValue := target - v
+		coopIndex := slices.Index(input[i+1:], nextValue)
+
+		if coopIndex == -1 {
+			continue
+		} else {
+
+			result = append(result, i)
+			result = append(result, i+coopIndex+1)
+			break
+		}
+
+	}
+
+	return result
+
+}
