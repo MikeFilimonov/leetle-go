08885b41b8fc53d1f54ee0effba9a404c9e7f7b7
MikeFilimonov <filimonov.michael@gmail.com>
2026-08-23 16:05:07 +0300

    adopted to initial task

--- concurrency/easy/easy-concurrency.go
+++ concurrency/easy/easy-concurrency.go
@@ -1,39 +1,35 @@
-package leetconcurrency
-
-import "fmt"
-
-type MetaScheduler struct {
+type Foo struct {
 	starter  chan struct{}
 	follower chan struct{}
 }
 
-func NewMetaScheduler() MetaScheduler {
-
-	return MetaScheduler{
+func NewFoo() *Foo {
+	return &Foo{
 		starter:  make(chan struct{}),
 		follower: make(chan struct{}),
 	}
 
 }
 
-func (ms *MetaScheduler) first() {
-
-	fmt.Println("first")
-	ms.starter <- struct{}{}
-
-}
-
-func (ms *MetaScheduler) second() {
-
-	<-ms.starter
-	fmt.Println("second")
-	ms.follower <- struct{}{}
+func (f *Foo) First(printFirst func()) {
+	// Do not change this line
+	printFirst()
+	f.starter <- struct{}{}
 
 }
 
-func (ms *MetaScheduler) third() {
+func (f *Foo) Second(printSecond func()) {
 
-	<-ms.follower
-	fmt.Println("third")
+	<-f.starter
+	/// Do not change this line
+	printSecond()
+	f.follower <- struct{}{}
 
+}
+
+func (f *Foo) Third(printThird func()) {
+
+	<-f.follower
+	// Do not change this line
+	printThird()
 }
