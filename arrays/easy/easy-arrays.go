2491ef24b50674ea2cff290f5e84d49feff07564
MikeFilimonov <filimonov.michael@gmail.com>
2026-08-31 22:30:14 +0300

    LCP

--- arrays/easy/easy-arrays.go
+++ arrays/easy/easy-arrays.go
@@ -2,6 +2,7 @@ package leetarrays
 
 import (
 	"slices"
+	"strings"
 )
 
 func TwoSum(input []int, target int) []int {
@@ -25,5 +26,46 @@ func TwoSum(input []int, target int) []int {
 	}
 
 	return result
+
+}
+
+func LongestCommonPrefix(strs []string) string {
+
+	if len(strs) == 0 {
+		return ""
+	}
+	if len(strs) == 1 {
+		return strs[0]
+	}
+
+	shorty := strs[0]
+	for _, v := range strs {
+		if len(v) < len(shorty) {
+			shorty = v
+		}
+	}
+
+	var sb strings.Builder
+
+	for i := 0; i < len(shorty); i++ {
+
+		starter := strs[0][i]
+
+		isCommonPrefix := true
+		for c := 1; c < len(strs); c++ {
+			isCommonPrefix = isCommonPrefix && (starter == strs[c][i])
+			if !isCommonPrefix {
+				break
+			}
+		}
+
+		if !isCommonPrefix {
+			break
+		}
+		sb.WriteByte(starter)
+
+	}
+
+	return sb.String()
 
 }
