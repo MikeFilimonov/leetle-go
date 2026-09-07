34ec0b27e8fc5b2c9b846b508cd0a83ec8a45b7c
MikeFilimonov <filimonov.michael@gmail.com>
2026-09-08 00:26:18 +0300

    refactored TwoSum

--- arrays/easy/easy-arrays.go
+++ arrays/easy/easy-arrays.go
@@ -1,30 +1,19 @@
 package leetarrays
 
-import (
-	"slices"
-)
-
 func TwoSum(input []int, target int) []int {
 
-	result := make([]int, 0)
+	buffer := make(map[int]int)
+	for k, v := range input {
 
-	for i, v := range input {
-
-		nextValue := target - v
-		coopIndex := slices.Index(input[i+1:], nextValue)
-
-		if coopIndex == -1 {
-			continue
-		} else {
-
-			result = append(result, i)
-			result = append(result, i+coopIndex+1)
-			break
+		another := target - v
+		if idx2, found := buffer[another]; found {
+			return []int{idx2, k}
 		}
+		buffer[v] = k
 
 	}
 
-	return result
+	return []int{}
 
 }
 
